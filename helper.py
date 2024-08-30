7cabbd3d4da6db34d5837fc242301b2559d3257b
33Arsenic75 <122697564+33Arsenic75@users.noreply.github.com>
2024-08-30 15:09:56 +0530

    .

--- helper.py
+++ helper.py
@@ -1,68 +1,6 @@
-# from itertools import product
-# import json
-# from hashing import hash_word
-
-
-# def reverse_phoneme_table(phoneme_table):
-#     reverse_table = {}
-#     for original, subs in phoneme_table.items():
-#         for sub in subs:
-#             if sub not in reverse_table:
-#                 reverse_table[sub] = []
-#             reverse_table[sub].append(original)
-#     return reverse_table
-
-
-# def generate_original_possibilities(asr_word, phoneme_table, all_words):
-#     reverse_table = reverse_phoneme_table(phoneme_table)
-#     possible_substitutions = []
-#     for char in asr_word:
-#         if char in reverse_table:
-#             possible_substitutions.append(reverse_table[char] + [char])
-#         else:
-#             possible_substitutions.append([char])
-#     all_combinations = product(*possible_substitutions)
-#     possible_words = ["".join(combo) for combo in all_combinations]
-#     result = [asr_word]
-#     for i in possible_words:
-#         hash_value = hash_word(i)
-#         if hash_value not in all_words:
-#             continue
-#         lst = all_words[hash_value]
-#         for j in lst:
-#             if i == j[0]:
-#                 result.append(i)
-#     return result
-
-
-# # Example usage
-# # with open("data/phoneme_table.json", "r") as f:
-# #     phoneme_table = json.load(f)
-
-# # with open("words.json", "r") as f:
-# #     all_words = json.load(f)
-
-# # asr_word = "GINGS"
-# # original_possibilities = generate_original_possibilities(
-# #     asr_word, phoneme_table, all_words
-# # )
-# # for i in original_possibilities:
-# #     hash_value = hash_word(i)
-# #     if hash_value not in all_words:
-# #         continue
-# #     lst = all_words[hash_value]
-# #     for j in lst:
-# #         if i == j[0]:
-# #             print(i)
-
-# # lst = ["ABC", "DEF"]
-# # sentence = " ".join(lst) + "X"
-# # print(sentence)
-
-import json
 from itertools import product
+import json
 from hashing import hash_word
-from copy import deepcopy
 
 
 def reverse_phoneme_table(phoneme_table):
@@ -75,52 +13,31 @@ def reverse_phoneme_table(phoneme_table):
     return reverse_table
 
 
-def generate_neighbors(word, reverse_table):
-    neighbors = []
-    for i, char in enumerate(word):
-        if char in reverse_table:
-            for original in reverse_table[char]:
-                neighbor = word[:i] + original + word[i + 1 :]
-                neighbors.append(neighbor)
-    return neighbors
-
-
 def local_beam_search(asr_word, phoneme_table, all_words, beam_width, max_iterations):
     reverse_table = reverse_phoneme_table(phoneme_table)
+    possible_substitutions = [asr_word]
+    for char in asr_word:
+        if char in reverse_table:
+            possible_substitutions.append(reverse_table[char] + [char])
+        else:
+            possible_substitutions.append([char])
+    all_combinations = product(*possible_substitutions)
+    possible_words = ["".join(combo) for combo in all_combinations]
+    frequency_list = []  # List to store words with their frequencies
+    result = []
+    for i in possible_words:
+        hash_value = hash_word(i)
+        if hash_value not in all_words:
+            continue
+        lst = all_words[hash_value]
+        for j in lst:
+            if i == j[0]:
+                result.append(i)
+                frequency_list.append((i, j[1]))  # Add (word, frequency) to frequency list
 
-    # Initialize beam with the original ASR word
-    beam = [asr_word]
+    # Sort frequency_list by occurrences in descending order
+    frequency_list.sort(key=lambda x: x[1], reverse=True)
 
-    for _ in range(max_iterations):
-        candidates = []
-        for word in beam:
-            # Generate neighbors using the reversed phoneme table
-            neighbors = generate_neighbors(word, reverse_table)
-            candidates.extend(neighbors)
-
-        # Remove duplicates and limit to valid words
-        candidates = list(set(candidates))
-        valid_candidates = [
-            word
-            for word in candidates
-            if hash_word(word) in all_words
-            and any(word == w[0] for w in all_words[hash_word(word)])
-        ]
-
-        # If no valid candidates, break the loop
-        if not valid_candidates:
-            break
-
-        # Sort candidates by their frequency in all_words
-        sorted_candidates = sorted(
-            valid_candidates,
-            key=lambda w: max(
-                count for word, count in all_words[hash_word(w)] if word == w
-            ),
-            reverse=True,
-        )
-
-        # Select top beam_width candidates
-        beam = sorted_candidates[:beam_width]
-
-    return beam
+    # Extract top k words
+    top_k_words = [word for word, freq in frequency_list[:beam_width]]
+    return top_k_words
--- solution.py
+++ solution.py
@@ -1,61 +1,3 @@
-# import json
-# from helper import generate_original_possibilities
-
-
-# class Agent(object):
-#     def __init__(self, phoneme_table, vocabulary, word_file="words.json") -> None:
-#         """
-#         Your agent initialization goes here. You can also add code but don't remove the existing code.
-#         """
-#         self.phoneme_table = phoneme_table
-#         self.vocabulary = vocabulary
-#         self.best_state = None
-#         with open(word_file, "r") as f:
-#             self.word_file = json.load(f)
-
-#     def asr_corrector(self, environment):
-#         """
-#         Your ASR corrector agent goes here. Environment object has the following important members.
-#         - environment.init_state: Initial state of the environment. This is the text that needs to be corrected.
-#         - environment.compute_cost: A cost function that takes a text and returns a cost. E.g., environment.compute_cost("hello") -> 0.5
-
-#         Your agent must update environment.best_state with the corrected text discovered so far.
-#         """
-#         self.best_state = environment.init_state
-#         current_cost = environment.compute_cost(self.best_state)
-#         words = self.best_state.split()
-#         print(self.best_state)
-#         print()
-
-#         def compute_cost_lst(lst):
-#             sentence = " ".join(lst)
-#             return environment.compute_cost(sentence)
-
-#         temp_lst = words.copy()
-
-#         f = True
-#         while f:
-#             f = False
-#             for i in range(len(temp_lst)):
-#                 asr_word = temp_lst[i]
-#                 original_possibilities = generate_original_possibilities(
-#                     asr_word, self.phoneme_table, self.word_file
-#                 )
-#                 for possibility in original_possibilities:
-#                     # Temporarily replace the word and compute the new cost
-#                     words[i] = possibility
-#                     new_cost = compute_cost_lst(words)
-#                     # Update if the new cost is lower
-#                     if new_cost < current_cost:
-#                         f = True
-#                         current_cost = new_cost
-#                         self.best_state = " ".join(words)
-#                     else:
-#                         # Revert if no improvement
-#                         words[i] = asr_word
-
-#         print(self.best_state)
-
 import json
 from helper import local_beam_search
 
@@ -84,59 +26,49 @@ class Agent(object):
 
     def asr_corrector(self, environment):
         """
-        ASR corrector agent using local beam search with sliding window.
+        Your ASR corrector agent goes here. Environment object has the following important members.
+        - environment.init_state: Initial state of the environment. This is the text that needs to be corrected.
+        - environment.compute_cost: A cost function that takes a text and returns a cost. E.g., environment.compute_cost("hello") -> 0.5
+
+        Your agent must update environment.best_state with the corrected text discovered so far.
         """
         self.best_state = environment.init_state
         current_cost = environment.compute_cost(self.best_state)
         words = self.best_state.split()
-        print("Initial state:", self.best_state)
-        print("Initial cost:", current_cost)
+        print(self.best_state)
+        print()
 
         def compute_cost_lst(lst):
             sentence = " ".join(lst)
             return environment.compute_cost(sentence)
 
-        improved = True
-        while improved:
-            improved = False
-            for i in range(len(words) - self.window_size + 1):
-                window = words[i : i + self.window_size]
-                original_window = window.copy()
+        temp_lst = words.copy()
+        possible_changes = []*len(temp_lst)
+        for i in range(len(temp_lst)):
+            possible_changes[i] = local_beam_search(
+                    temp_lst[i],
+                    self.phoneme_table,
+                    self.word_file,
+                    self.beam_width,
+                    self.max_iterations,)
 
-                # Generate candidates for each word in the window
-                candidates = [
-                    local_beam_search(
-                        word,
-                        self.phoneme_table,
-                        self.word_file,
-                        self.beam_width,
-                        self.max_iterations,
-                    )
-                    for word in window
-                ]
-
-                # Generate all combinations of candidates
-                import itertools
-
-                combinations = list(itertools.product(*candidates))
-
-                best_combination = window
-                best_combination_cost = current_cost
-
-                for combination in combinations:
-                    words[i : i + self.window_size] = combination
+        f = True
+        while f:
+            f = False
+            for i in range(len(temp_lst)):
+                asr_word = temp_lst[i]
+                original_possibilities = possible_changes[i]
+                for possibility in original_possibilities:
+                    # Temporarily replace the word and compute the new cost
+                    words[i] = possibility
                     new_cost = compute_cost_lst(words)
-                    if new_cost < best_combination_cost:
-                        best_combination = list(combination)
-                        best_combination_cost = new_cost
+                    # Update if the new cost is lower
+                    if new_cost < current_cost:
+                        f = True
+                        current_cost = new_cost
+                        self.best_state = " ".join(words)
+                    else:
+                        # Revert if no improvement
+                        words[i] = asr_word
 
-                if best_combination != original_window:
-                    improved = True
-                    words[i : i + self.window_size] = best_combination
-                    current_cost = best_combination_cost
-                    self.best_state = " ".join(words)
-                else:
-                    words[i : i + self.window_size] = original_window
-
-        print("Final state:", self.best_state)
-        print("Final cost:", current_cost)
+        print(self.best_state)
